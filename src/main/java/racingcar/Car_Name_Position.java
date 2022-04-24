41c342dfd63320ad297f4e8a8e3ec47e7f4a66d4
JangJaehoon <jjhgwx@gmail.com>
2022-04-24 22:22:30 +0900

    이름 전진범위 이름길이제한메시지

---
+++ src/main/java/racingcar/Car_Name_Position.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car_Name_Position {
+    private static final int min_length_name = 1;
+    public static final int max_length_name = 5;
+    public static final int min_move = 4;
+    public static final int min_range_move = 0;
+    public static final int max_range_move = 9;
+    public static final String road = "-";
+    public static final String name_length_info = "[ERROR] The name of car should be from 1 character to 5 characters.";
+
+
+
+}
