d44f7f271bb93f74c596a3c60bc5d6ce5c8b7fe8
JangJaehoon <jjhgwx@gmail.com>
2022-04-24 23:31:43 +0900

    경기 회수에 대한 유효성 처리 클래스

---
+++ src/main/java/racingcar/PlayRecord.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class PlayRecord {
+
+    public static final String number_exception
+            = "[ERROR] Try count should be number(int)";
+    public static final String plus_exception
+            = "[ERROR] Try count should be plus number";
+    private Integer round = 0;
+
+}
